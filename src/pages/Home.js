aa625faca1662369e7471b52dff1506270a4e1d4
zhenja-ulchak <super-zhenja@ukr.net>
2022-10-27 12:47:53 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+export default class Home extends  Component{
+    render(){
+        return(
+            <div style={{marginTop:'100px'}}><h1>home</h1></div>
+        )
+    }
+}
